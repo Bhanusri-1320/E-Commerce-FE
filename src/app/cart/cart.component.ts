337c229118580acf2e10f32d4d6edcb01a5412a4
Bhanusri-1320 <bhanusri.gangadevi@proclink.com>
2025-06-24 11:10:47 +0530

    cart working

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../service/product.service';
+import { IonCard } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [IonCard, ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
 export class CartComponent {
-   id:string | null = null;
-   constructor(private route:ActivatedRoute)
+   id!: string;
+   product:any;
+   constructor(private route:ActivatedRoute, private productService: ProductService)
    {}
    ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
     if (id) {
       this.id = id;
       console.log('CartComponent initialized with ID:', this.id);
+      this.getProductDetails(this.id);
     }
+   }
+   getProductDetails(id:string) {
+      this.productService.getProductById(this.id).subscribe(
+        (data) => {
+          this.product = data;
+          console.log('Product Details:', this.product);
+        }
+      )
    }
 }
